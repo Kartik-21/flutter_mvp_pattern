e8ace30f7cf338eb8b07e61de8173e8b47e589c8
Kartik-21 <kartikjain2926@gmail.com>
2021-01-23 15:33:25 +0530

    init

--- lib/HomePresenter.dart
+++ lib/HomePresenter.dart
@@ -1,14 +1,13 @@
 import 'package:flutter_mvp_pattern/HomeScreen.dart';
 
 class HomePresenter {
-  HomeScreenState _view;
+  HomeScreenState _view; //it take view object
 
-  HomePresenter(this._view);
-
+  HomePresenter(this._view); //and assign into presenter
 
   Future getData() {
     /// get variables and objects form view
- // _view.objects
+    // _view.objects
     print('ok we got');
   }
 }
